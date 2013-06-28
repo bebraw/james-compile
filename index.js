822f3fd978a9cf858af709ef6e922dd53128dd60
Juho Vepsalainen <bebraw@gmail.com>
2013-06-28 21:44:20 +0300

    Remove compiler from options before compiling

--- index.js
+++ index.js
@@ -2,6 +2,9 @@ var james = require('james');
 
 
 module.exports = function(options) {
+    var compiler = options.compiler;
+    delete options.compiler;
+
     return james.createStream(function(file, callback) {
         callback(options.compiler.compile(file, options)(options.context));
     });
